9b5bcdb98d09055a0ee01d3b94257d25ac0bc012
minsung <mingsung521@gmail.com>
2021-09-10 18:25:13 +0900

    router & btn finished

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 import 'package:its_soccer_time/src/elements/floatingmenu.dart';
 
 import 'data/font.dart';
@@ -14,14 +15,71 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "It's soccertime",
-          style: TextStyle(fontFamily: MyFonts.gmarketsans),
+          "It's soccer time",
+          style: TextStyle(
+            fontFamily: MyFonts.gmarketsans,
+            fontSize: 18,
+            letterSpacing: -0.25,
+          ),
         ),
-
-        // shape: Border(bottom: BorderSide(color: Colors.black, width: 2)),
       ),
-      floatingActionButton: FloatingMenu(),
-      // floatingActionButton: FloatingMenu(),
+      floatingActionButton: SpeedDial(
+        backgroundColor: Colors.transparent,
+        activeBackgroundColor: Colors.transparent,
+        useRotationAnimation: true,
+        // SvgPicture.asset("assets/svg/icons/soccer-ball.svg");
+        elevation: 0.0,
+        child: Container(
+          color: Colors.transparent,
+          child: Image.asset('assets/svg/icons/soccer-ball.png'),
+        ),
+        children: [
+          SpeedDialChild(
+            onTap: () {
+              Get.offAllNamed("/settings");
+            },
+            child: Icon(Icons.settings),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            foregroundColor: Colors.grey,
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.favorite),
+            onTap: () {
+              Get.offAllNamed("/favorites");
+            },
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            foregroundColor: Colors.grey,
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.notifications),
+            onTap: () {
+              Get.offAllNamed("/");
+            },
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.feed),
+            backgroundColor: Colors.transparent,
+            foregroundColor: Colors.grey,
+            elevation: 0.0,
+            onTap: () {
+              Get.offAllNamed("/news");
+            },
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.calendar_today),
+            backgroundColor: Colors.transparent,
+            onTap: () {
+              Get.offAllNamed("/matches");
+            },
+            foregroundColor: Colors.grey,
+            elevation: 0.0,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/src/pages/matches.dart
+++ lib/src/pages/matches.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:get/get.dart';
 import 'package:its_soccer_time/src/data/font.dart';
 import 'package:its_soccer_time/src/elements/floatingmenu.dart';
 
@@ -9,16 +11,72 @@ class Matches extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "경기일정",
-            style: TextStyle(fontFamily: MyFonts.gmarketsans),
-          ),
-
-          // shape: Border(bottom: BorderSide(color: Colors.black, width: 2)),
+      appBar: AppBar(
+        title: Text(
+          "경기일정",
+          style: TextStyle(fontFamily: MyFonts.gmarketsans),
         ),
-        floatingActionButton: FloatingMenu()
-        // floatingActionButton: FloatingMenu(),
-        );
+
+        // shape: Border(bottom: BorderSide(color: Colors.black, width: 2)),
+      ),
+      floatingActionButton: SpeedDial(
+        backgroundColor: Colors.transparent,
+        activeBackgroundColor: Colors.transparent,
+        useRotationAnimation: true,
+        // SvgPicture.asset("assets/svg/icons/soccer-ball.svg");
+        elevation: 0.0,
+        child: Container(
+          color: Colors.transparent,
+          child: Image.asset('assets/svg/icons/soccer-ball.png'),
+        ),
+        children: [
+          SpeedDialChild(
+            onTap: () {
+              Get.offAllNamed("/settings");
+            },
+            child: Icon(Icons.settings),
+            backgroundColor: Colors.transparent,
+            foregroundColor: Colors.grey,
+            elevation: 0.0,
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.favorite),
+            onTap: () {
+              Get.offAllNamed("/favorites");
+            },
+            backgroundColor: Colors.transparent,
+            foregroundColor: Colors.grey,
+            elevation: 0.0,
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.notifications),
+            onTap: () {
+              Get.offAllNamed("/");
+            },
+            foregroundColor: Colors.grey,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.feed),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            onTap: () {
+              Get.offAllNamed("/news");
+            },
+            foregroundColor: Colors.grey,
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.calendar_today),
+            backgroundColor: Colors.transparent,
+            onTap: () {
+              Get.offAllNamed("/matches");
+            },
+            elevation: 0.0,
+          ),
+        ],
+      ),
+      // floatingActionButton: FloatingMenu(),
+    );
   }
 }
